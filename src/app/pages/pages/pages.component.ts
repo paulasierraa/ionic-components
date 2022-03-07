d091363e77b4c805128dd927723d917b402d7a68
Paula Sierra <PaulaCode01@gmail.com>
2022-03-07 09:22:34 -0500

    Lista para páginas

--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -7,6 +12,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'logo-apple-appstore',
+      name: 'Alert',
+      redirectTo: '/alert'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
